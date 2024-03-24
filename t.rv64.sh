c0f0bc7274db305c2a559fce48370ec66f797e9b
Allen Guo <guosonglin001@gmail.com>
2024-03-24 14:41:09 +0000

    wip

--- t.rv64.sh
+++ t.rv64.sh
@@ -1,12 +1,36 @@
+# args=(
+#   -DCMAKE_BUILD_TYPE=Release
+#   -DCMAKE_INSTALL_PREFIX=$PWD/build/install
+#   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+#   -DENABLE_ICD=OFF
+#   -DENABLE_SLEEF=OFF
+#   -DENABLE_HWLOC=OFF
+#   -DENABLE_TESTS=OFF
+#   # -DENABLE_EXAMPLES=OFF
+#   -DENABLE_PRINTF_IMMEDIATE_FLUSH=OFF
+#   -DCLANG_MARCH_FLAG="-mcpu="
+#   -DLLC_HOST_CPU="generic-rv64"
+#   -DLLC_TRIPLE="riscv64-unknown-linux-gnu"
+#   -S$PWD -B$PWD/build -GNinja
+# )
+# cmake "${args[@]}"
+# cmake --build $PWD/build --target all
+# cmake --build $PWD/build --target install
+
+###############################################################################
+
 args=(
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Debug
   -DCMAKE_INSTALL_PREFIX=$PWD/build/install
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  -DCMAKE_C_COMPILER=/usr/bin/clang
+  -DCMAKE_CXX_COMPILER=/usr/bin/clang++
   -DENABLE_ICD=OFF
   -DENABLE_SLEEF=OFF
   -DENABLE_HWLOC=OFF
   -DENABLE_TESTS=OFF
   # -DENABLE_EXAMPLES=OFF
+  -DDEVELOPER_MODE=ON
   -DENABLE_PRINTF_IMMEDIATE_FLUSH=OFF
   -DCLANG_MARCH_FLAG="-mcpu="
   -DLLC_HOST_CPU="generic-rv64"
@@ -15,7 +39,6 @@ args=(
 )
 cmake "${args[@]}"
 
-cmake --build $PWD/build --target all
 cmake --build $PWD/build --target install
 
 ###############################################################################
