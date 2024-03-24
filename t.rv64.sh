acf64c3cc74623c0d0f1fbe9ef798dff88035acc
Allen Guo <guosonglin001@gmail.com>
2024-03-24 13:33:00 +0000

    wip

--- t.rv64.sh
+++ t.rv64.sh
@@ -1,14 +1,17 @@
-#
-cmake \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=$PWD/build/install \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DENABLE_ICD=OFF \
-  -DENABLE_SLEEF=OFF \
-  -DCLANG_MARCH_FLAG="-mcpu=" \
-  -DLLC_HOST_CPU="generic-rv64" \
-  -DLLC_TRIPLE="riscv64-unknown-linux-gnu" \
+args=(
+  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_INSTALL_PREFIX=$PWD/build/install
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  -DENABLE_ICD=OFF
+  -DENABLE_SLEEF=OFF
+  -DENABLE_HWLOC=OFF
+  -DENABLE_PRINTF_IMMEDIATE_FLUSH=OFF
+  -DCLANG_MARCH_FLAG="-mcpu="
+  -DLLC_HOST_CPU="generic-rv64"
+  -DLLC_TRIPLE="riscv64-unknown-linux-gnu"
   -S $PWD -B $PWD/build
+)
+cmake "${args[@]}"
 
 cmake --build $PWD/build --target all
 cmake --build $PWD/build --target install
